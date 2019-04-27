4bd3bbc0ea578eb47f79eb1d102e44efcf11d142
陈情 <chenqing@cq.local>
2019-04-27 15:25:51 +0800

    beian

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,65 +1,78 @@
 <style>
-.front .container{
-    padding-bottom: 80px;
+.front .container {
+  padding-bottom: 80px;
 }
-.front .ivu-layout-header{
-    background: #2d8cf0 !important;
-    color: #fff;
-    display: flex;
-    justify-content: space-between;
+.front .ivu-layout-header {
+  background: #2d8cf0 !important;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
 }
-.front .ivu-layout-footer{
-    height: 80px;
-    bottom: 0;
-    width: 100%;
-    background: #2d8cf0 !important;
-    color: #fff !important;
-    text-align: center;
-    line-height: 80px;
-    padding: 0 !important;
+.front .ivu-layout-footer {
+  height: 80px;
+  bottom: 0;
+  width: 100%;
+  line-height: 30px;
+  background: #2d8cf0 !important;
+  color: #fff !important;
+  text-align: center;
+  padding-top:10px;
 }
-.front .ivu-layout-footer a{
-    color: #fff;
+.front .ivu-layout-footer a {
+  color: #fff;
 }
-.front .ivu-menu-item a{
-    width: 100%;
-    height: 100%;
-    color: #fff;
+.front .ivu-layout-footer a img{
+  vertical-align: middle;
 }
-.front .ivu-menu-item a:hover{
-    color: #fff;
+.front .ivu-menu-item a {
+  width: 100%;
+  height: 100%;
+  color: #fff;
 }
-
-
+.front .ivu-menu-item a:hover {
+  color: #fff;
+}
 </style>
 
 <template>
-   <Layout class="front">
-        <Header>
-            <div class="title"><h2>安然</h2></div>
-            <div class="menu">
-                <Menu mode="horizontal" :theme="theme1" active-name="1">
-        <MenuItem name="2">
-         <nuxt-link to="/front/index">
-            <Icon type="md-bookmarks" />
-            前端技术
-        </nuxt-link>    
-        </MenuItem>
-         <MenuItem name="2">
-         <nuxt-link to="/front/aboutMe">
-            <Icon type="ios-people" />
-             关于我
-        </nuxt-link>    
-        </MenuItem>
-    </Menu>
-            </div>
-        </Header>
-        <Content> 
-            <nuxt/>
-             <Footer>© 2019 - Bonnie Blog - <a href="http://www.miitbeian.gov.cn/">蜀ICP备19008745号-1</a></Footer>
-        </Content>
-       
-    </Layout>
+  <Layout class="front">
+    <Header>
+      <div class="title">
+        <h2>安然</h2>
+      </div>
+      <div class="menu">
+        <Menu mode="horizontal" :theme="theme1" active-name="1">
+          <MenuItem name="2">
+            <nuxt-link to="/front/index">
+              <Icon type="md-bookmarks"/>前端技术
+            </nuxt-link>
+          </MenuItem>
+          <MenuItem name="2">
+            <nuxt-link to="/front/aboutMe">
+              <Icon type="ios-people"/>关于我
+            </nuxt-link>
+          </MenuItem>
+        </Menu>
+      </div>
+    </Header>
+    <Content>
+      <nuxt/>
+      <Footer>
+        <a
+          target="_blank"
+          href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51010602000915"
+          style=“display:block”
+        >
+          <img src="~assets/images/ba.png">
+          川公网安备 51010602000915号
+        </a>
+        <p>
+          © 2019 - Bonnie Blog -
+          <a href="http://www.miitbeian.gov.cn/">蜀ICP备19008745号-1</a>
+        </p>
+      </Footer>
+    </Content>
+  </Layout>
 </template>
 <script>
 export default {
@@ -68,8 +81,8 @@ export default {
       theme1: "primary"
     };
   },
-  created(){
-      this.$router.push("/front/index");
+  created() {
+    this.$router.push("/front/index");
   }
 };
 </script>
